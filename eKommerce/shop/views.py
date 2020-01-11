f8e202070d432746b219a33cc2f6e2c243c3b1b8
Prajjwal Mishra <prajjwalgeek@gmail.com>
2020-01-11 15:59:24 +0530

    laid pipelines for shop app

--- eKommerce/shop/views.py
+++ eKommerce/shop/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'shop/index.html')
     # return HttpResponse("<h1>Index Shop</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About EKommerce</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Contact eKommerce</h1>")
+
+def tracker(request):
+    return HttpResponse("<h1>Tracker eKommerce</h1>")
+
+def search(request):
+    return HttpResponse("<h1>Search eKommerce</h1>")
+
+def productView(request):
+    return HttpResponse("<h1>Product View eKommerce</h1>")
+
+def checkout(request):
+    return HttpResponse("<h1>Checkout eKommerce</h1>")
